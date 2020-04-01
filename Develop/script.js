fd6e34a74c4ec1b1059134e7311b1527072a26dc
Katherine Jones <kejone1414@gmail.com>
2020-04-01 17:48:59 -0400

    commented out timeblock for now

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,3 @@
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 // WHEN I click into a timeblock
 // THEN I can enter an event
 // WHEN I click the save button for that timeblock
@@ -13,27 +9,28 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 // timeblocks 8-5, because who actually gets a 9-5 anymore?
-var workHours = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"]
-var milHours = ["08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"]
+var workHours = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+var milHours = ["08", "09", "10", "11", "12", "13", "14", "15", "16", "17"]
 var currentHour = moment().format("MMMM Do YYYY HH")
-console.log(currentHour)
 
 workHours.forEach(function addHourstoPage(item, index){
     var newRow = $("<div>").addClass("row");
+    // var timeblock = $("<div>").addClass("time-block");
     var newHour = $("<div>").text(item.toUpperCase());
     $(newHour).addClass("hour");
     $(newHour).attr("data-time", moment().format("MMMM Do YYYY") + " " + milHours[index])
-    var timeblock = $("<div>").addClass("time-block");
     var textArea = $("<p>").addClass("textarea");
+    // remove once ready for prod
     $(textArea).text("test");
+    var saveBtn = $("<button>").addClass("saveBtn");
 
     $(".container").append(newRow);
     $(newRow).append(newHour);
-    $(newRow).append(timeblock);
-    $(timeblock).append(textArea);
+    // $(timeblock).append(newHour);
+    $(newRow).append(textArea);
+    $(newRow).append(saveBtn);
 
     var hourData = $(newHour).attr("data-time")
-    console.log(hourData)
 
     if (hourData === currentHour) {
         $(textArea).addClass("present");
