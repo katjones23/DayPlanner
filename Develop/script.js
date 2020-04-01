231ab6243a9503b64462c39643da6395b70246a7
Katherine Jones <kejone1414@gmail.com>
2020-04-01 17:39:45 -0400

    time comparison logic working

--- Develop/script.js
+++ Develop/script.js
@@ -13,19 +13,35 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 // timeblocks 8-5, because who actually gets a 9-5 anymore?
-var workHours = ["8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+var workHours = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"]
+var milHours = ["08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"]
+var currentHour = moment().format("MMMM Do YYYY HH")
+console.log(currentHour)
 
-workHours.forEach(function (item){
+workHours.forEach(function addHourstoPage(item, index){
     var newRow = $("<div>").addClass("row");
-    var newHour = $("<div>").text(item);
+    var newHour = $("<div>").text(item.toUpperCase());
     $(newHour).addClass("hour");
+    $(newHour).attr("data-time", moment().format("MMMM Do YYYY") + " " + milHours[index])
     var timeblock = $("<div>").addClass("time-block");
-    var textArea = $("<div>").addClass("textarea");
+    var textArea = $("<p>").addClass("textarea");
+    $(textArea).text("test");
 
     $(".container").append(newRow);
     $(newRow).append(newHour);
-    $(newHour).append(timeblock);
-    $(timeblock).append(textArea)
+    $(newRow).append(timeblock);
+    $(timeblock).append(textArea);
+
+    var hourData = $(newHour).attr("data-time")
+    console.log(hourData)
+
+    if (hourData === currentHour) {
+        $(textArea).addClass("present");
+    } else if (hourData < currentHour) {
+        $(textArea).addClass("past");
+    } else if (hourData > currentHour) {
+        $(textArea).addClass("future");
+    };
 });
 
     // potential classes needed:
