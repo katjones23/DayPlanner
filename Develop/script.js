39e3e5425c46abf293499757db653a578fefac23
Katherine Jones <kejone1414@gmail.com>
2020-04-01 16:09:10 -0400

    added hour rows to timeblock section (no inner text area yet)

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,3 @@
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
 // WHEN I view the timeblocks for that day
@@ -14,3 +11,23 @@
 
 // current day under header
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+// timeblocks 8-5, because who actually gets a 9-5 anymore?
+var workHours = ["8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+
+workHours.forEach(function (item){
+    var newRow = $("<div>").addClass("row");
+    var timeblock = $("<div>").addClass("time-block");
+    var newHour = $("<section>").text(item);
+    $(newHour).addClass("hour");
+
+    $(".container").append(newRow);
+    $(newRow).append(timeblock);
+    $(timeblock).append(newHour);
+});
+
+    // potential classes needed:
+    // .time-block
+    // .row
+    // .hour
+    // .textarea
